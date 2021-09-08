54e911d430c12cb18bf6b98dfe8162a3aa27ee9f
rayne <raynechiang5235@gmail.com>
2021-09-08 03:46:55 +0100

    [Add more comments on LSTM stop]

--- lstm_stop.py
+++ lstm_stop.py
@@ -1,10 +1,7 @@
 from tkinter import *
 
-import numpy
 import pandas as pd
-import numpy as np
 import os
-import re
 import nltk
 
 nltk.download('punkt')
@@ -22,10 +19,13 @@ from tensorflow.keras.models import Sequential
 
 import timeit
 
-'''
-Filter all the records for Test
-'''
+
 def testFilter(df):
+    '''
+    Filter all the records for Test or the model accuracy will be unreasonably high
+    :param df:
+    :return: dataframe without test records
+    '''
     for index, row in df.iterrows():
         des = row['Description  ']
         if (des.find('TEST') != -1 or des.find('test') != -1 or des.find('Test') != -1 or des.isnumeric()) and row[
@@ -34,65 +34,78 @@ def testFilter(df):
     df = df.drop_duplicates()
     return df
 
-'''
-Extract words with Lemmatization
-'''
+
 def extract_word(df):
+    '''
+    Extract words with to get the base form of the words
+    :param df:
+    :return:
+    '''
     lemmatizer = WordNetLemmatizer()
     word_list = []
     for index, row in df.iterrows():
         des = row['Description  ']
         des = des.lower()
+        # split the sentence of Description into tokens
         tokens = word_tokenize(des)
         words = [lemmatizer.lemmatize(word) for word in tokens if word.isalpha()]  # Lemmatization
         if len(words) == 0:
-            df = df.drop(index)
+            df = df.drop(index)  # if there is no words just drop it
         else:
             word_list.append(words)
     df['Words'] = word_list
     return df
 
-'''
-Padding text to sequence
-size: vocab_size
-len: max_len
-'''
+
 def text_to_sequence(df, size, len):
+    '''
+    Padding text to sequence to transform each text in texts to a sequence of integers
+    :param df: the source of the padding sequence
+    :param size: vocab_size, the size of the sequence
+    :param len: max_len
+    :return:
+    '''
     # Tokenize the mails
     tok = Tokenizer(num_words=size)
-    nna_report = extract_word(df)  # 1442 left with words
+    nna_report = extract_word(df)
     nna_report.reset_index(drop=True, inplace=True)
     y = nna_report['Flag'].map({'Yes': 1, 'No': 0}).values
     tok.fit_on_texts(nna_report['Words'])
-    # Use text_to_sequence to convert it into vectors
+    # Use text_to_sequence to convert it into sequence
     sequences = tok.texts_to_sequences(nna_report['Words'])
-
     # pad seqence to create a matrix of equal length mails
     X = sequence.pad_sequences(sequences, maxlen=len)
     return X, y, nna_report['Words']
 
-'''
-Train process
-'''
+
 def train():
+    '''
+    Train the model
+    '''
+    # get the start time
     start = timeit.default_timer()
+    # read all reports as the training database
     dir_name = os.path.dirname(__file__)
     all_report = pd.read_excel(dir_name + '/input/Stop App Reports no password.xlsx', sheet_name='All Reports - 4022')
     nna_report = all_report.dropna().copy()  # 2978 records left, use copy leaves the origin data unchanged
     nna_report['Description  '] = nna_report['Description  '].astype(str)
-    nna_report = testFilter(nna_report)  # 1459 left
 
-    vocab_size = 10000
-    max_len = 250
+    # nna_report = testFilter(nna_report)  # only use to see the performance of the model
+    vocab_size = 10000  # initial sequence size
+    max_len = 250  # initial description string size
 
-    X, y, df = text_to_sequence(nna_report[:1200], vocab_size, max_len)
+    # get the training data, X is the input sequence, y is the label. df is the training word List
+    X, y, df = text_to_sequence(nna_report, vocab_size, max_len)
+    # save the training word List for compare
     df.to_csv(dir_name + '/test/training_data.csv')
 
     checkpoint_filepath = dir_name + '/content/tmp/checkpoint'
+    # create the model
     model = Sequential()
+    # set the Embedding layer
     model.add(Embedding(vocab_size, 200, input_length=max_len))
-    model.add(LSTM(32))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(LSTM(32))  # create LSTM model with one LSTM layer
+    model.add(Dense(1, activation='sigmoid'))  # padding the output with sigmoid function
     model.summary()
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=checkpoint_filepath,
@@ -102,6 +115,7 @@ def train():
         save_best_only=True)
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # set early stoppin if loss did not decrease in 10 rounds then stop
     earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0)
     model.fit(X, y, validation_split=0.2, shuffle=True, epochs=30, batch_size=64,
               callbacks=[earlyStopping, model_checkpoint_callback])
@@ -112,55 +126,65 @@ def train():
     label.configure(text="Training Finished in " + Used_time + " seconds")
     print('Time: ', stop - start)
 
-'''
-Generate one test case for display
-'''
+
 def generate_test_dataset():
+    '''
+    Generate one test case for display
+    :return:
+    '''
     T.delete(1.0, END)
     dir_name = os.path.dirname(__file__)
     all_report = pd.read_excel(dir_name + '/input/Stop App Reports no password.xlsx', sheet_name='All Reports - 4022')
-    nna_report = all_report.dropna().copy()  # 2978 records left, use copy leaves the origin data unchanged
-    select_records = nna_report[1200:].sample(1)
+    nna_report = all_report.dropna().copy()  # use copy leaves the origin data unchanged
+    select_records = nna_report.sample(1)
     select_records = select_records['Description  '].values[0]
     T.insert(tk.END, select_records)
     str_test.set(select_records)
     label.configure(text="Ready for test")
 
-'''
-Generate wordlist from test case
-'''
+
 def generate_wordlist(str):
+    '''
+    Generate wordlist from test case
+    '''
     lemmatizer = WordNetLemmatizer()
     str = str.lower()
     tokens = word_tokenize(str)
     words = [lemmatizer.lemmatize(word) for word in tokens if word.isalpha()]  # Lemmatization
     return words
 
-'''
-Test function for display result
-'''
+
 def test():
+    '''
+    classify test case
+    :return: test result
+    '''
     dir_name = os.path.dirname(__file__)
     str = str_test.get()
     print(str)
+    # if string is numeric -> human should not look
     if str.isnumeric():
         label.configure(text="Human not look")
         return
     else:
+        # load the trained model
         load_model = keras.models.load_model(dir_name + "/model/my_model")
+        # extract words from string by Lemmatization
         word_list = generate_wordlist(str)
         print(word_list)
         if len(word_list) == 0:
+            # if there is no words can be extract -> human should not look
             label.configure(text="Human not look")
             return
         df = pd.read_csv(dir_name + '/test/training_data.csv')
         tok = Tokenizer(num_words=10000)
         df = df.append({'Words': word_list}, ignore_index=True)
         tok.fit_on_texts(df['Words'])
+        # fit the sequence of test on the training wordlist to get sequence weights
         sequences = tok.texts_to_sequences(df['Words'])
         test_data = sequence.pad_sequences(sequences, maxlen=250)
-        print(len(test_data))
-        probs = load_model.predict_proba(test_data)
+        probs = load_model.predict(test_data)
+        # set probability threshold
         thresh = 0.1
         pred = [1 if prob > thresh else 0 for prob in probs]
         print(probs[-1])
@@ -170,9 +194,8 @@ def test():
             label.configure(text="Human should look")
 
 
-
-
 if __name__ == '__main__':
+    # create python GUI with Tkinter
     root = tk.Tk()
     root.title("Stop App")
     root.geometry('500x300')
@@ -182,17 +205,22 @@ if __name__ == '__main__':
     label = tk.Label(root, text="Ready", bg='white', fg='black', font=('Arial', 12), width=30, height=2)
     label.pack(padx=20, pady=20, side=BOTTOM)
 
+    # create Train button
     btn_train = tk.Button(frame,
                           text="Train",
                           command=train)
     btn_train.pack(padx=5, pady=10, side=LEFT)
 
+    # get test string from the String Var container
     str_test = tk.StringVar()
+
+    # create Generate Test Dataset button
     btn_generate_test = tk.Button(frame,
                                   text="Generate Test Dataset",
                                   command=generate_test_dataset)
     btn_generate_test.pack(padx=5, pady=10, side=LEFT)
 
+    # create Test Dataset button
     btn_train = tk.Button(frame,
                           text="Test",
                           command=test)
@@ -203,9 +231,7 @@ if __name__ == '__main__':
 
     root.mainloop()
 
-
-
-
+    # code to see the performance
     # fpr, tpr, threshold = metrics.roc_curve(y_test, y_prob)
     # roc_auc = metrics.auc(fpr, tpr)
     #
@@ -223,4 +249,3 @@ if __name__ == '__main__':
     # pred = [1 if prob > thresh else 0 for prob in y_prob]
     # print(metrics.accuracy_score(y_test, pred))
     # print('Confusion Matrix', confusion_matrix(y_test, pred))
-    # model.save("sst")
